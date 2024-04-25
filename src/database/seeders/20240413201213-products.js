df660a1ace9078f868603a08f04760c8862b6f27
Caroline Nunes <nunescaroline905@gmail.com>
2024-04-25 15:05:43 -0300

    fix: update file `products`

---
+++ src/database/seeders/20240413201213-products.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('products', [
+     {
+        id: 1001,
+        product_name: 'CAIXA DE COPO 200 ML',
+        description: '48 UNIDADES',
+      },
+      {
+        id: 1002,
+        product_name: 'AGUA 510 ML SEM GÁS',
+        description: '12 UNIDADES',
+      },
+      {
+        id: 1003,
+        product_name: 'AGUA 510 ML COM GÁS',
+        description: '12 UNIDADES',
+      },
+      {
+        id: 1004,
+        product_name: 'AGUA 1500 ML SEM GÁS',
+        description: '12 UNIDADES',
+      },
+      {
+        id: 1005,
+        product_name: 'AGUA 1500 ML COM GÁS',
+        description: '12 UNIDADES',
+      },
+      {
+        id: 1006,
+        product_name: 'AGUA 5 LITROS',
+        description: '4 UNIDADES',
+      },
+      {
+        id: 1007,
+        product_name: 'AGUA 20 LITROS',
+        description: '1 UNIDADE',
+      },
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
