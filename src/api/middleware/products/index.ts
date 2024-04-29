256cd9187e993780ae279db5f0deae0ac5b44ee6
Caroline Nunes <nunescaroline905@gmail.com>
2024-04-29 17:43:58 -0300

    feat: creating product middleware

---
+++ src/api/middleware/products/index.ts
@@ -0,0 +1,43 @@
+import { celebrate, Joi, Segments } from 'celebrate';
+
+export function validateGetId() {
+  return celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.number().required()
+    }
+  })
+}
+
+export function validateCreate() {
+  return celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      description: Joi.string().required(),
+      image: Joi.string().required()
+    }
+  })
+}
+
+
+export function validateUpdate() {
+  return celebrate({
+    [Segments.BODY]: {
+      id: Joi.number().required(),
+      name: Joi.string().required(),
+      description: Joi.string().required(),
+      image: Joi.string().required()
+    },
+    [Segments.PARAMS]: {
+      id: Joi.number().required()
+    }
+  })
+}
+
+
+export function validateDelete() {
+  return celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.number().required()
+    }
+  })
+}
