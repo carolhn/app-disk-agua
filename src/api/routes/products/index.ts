39cf398f17e1b1a457743a86c536ff2179110952
Caroline Nunes <nunescaroline905@gmail.com>
2024-04-29 17:44:51 -0300

    refactor: adding routes validate

--- src/api/routes/products/index.ts
+++ src/api/routes/products/index.ts
@@ -1,14 +1,18 @@
 import { Router } from 'express';
 import { ProductsController } from 'src/api/controllers/products';
+import { validateCreate, validateDelete, validateGetId, validateUpdate } from 'src/api/middleware/products';
 
 const productsRouter = Router();
 const productsController = new ProductsController();
 
 productsRouter.get('/', productsController.listAllProducts);
-productsRouter.get('/:id', productsController.productById);
 
-productsRouter.post('/', productsController.createProduct);
-productsRouter.put('/:id', productsController.updateProduct);
-productsRouter.delete('/:id', productsController.deleteProduct);
+productsRouter.get('/:id',validateGetId(), productsController.productById);
+
+productsRouter.post('/', validateCreate(), productsController.createProduct);
+
+productsRouter.put('/:id',validateUpdate(), productsController.updateProduct);
+
+productsRouter.delete('/:id', validateDelete(), productsController.deleteProduct);
 
 export default productsRouter;
