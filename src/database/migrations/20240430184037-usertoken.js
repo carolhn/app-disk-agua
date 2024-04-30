0fb755b5a63279447e3101d19797c85fa5757ab6
Caroline Nunes <nunescaroline905@gmail.com>
2024-04-30 16:02:34 -0300

    fix: update `user_tokens` migrations

--- src/database/migrations/20240430184037-usertoken.js
+++ src/database/migrations/20240430184037-usertoken.js
@@ -26,11 +26,13 @@ module.exports = {
       },
       created_at: {
         type: Sequelize.DATE,
-        allowNull: false
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       },
       updated_at: {
         type: Sequelize.DATE,
-        allowNull: false
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
   });
   },
