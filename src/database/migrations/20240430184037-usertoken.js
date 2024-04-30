c8f9c872cf8bad9974e5a74c4239fedf1b3d3721
Caroline Nunes <nunescaroline905@gmail.com>
2024-04-30 15:50:36 -0300

    feat: create `userToken` migration

---
+++ src/database/migrations/20240430184037-usertoken.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('user_tokens', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      token: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+  });
+  },
+
+  async down (queryInterface, Sequelize) {
+   await queryInterface.dropTable('user_tokens');
+  }
+};
