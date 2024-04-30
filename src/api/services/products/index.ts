c3fc952913f76887b64cd5e6b0e2206abc6cb7c1
Caroline Nunes <nunescaroline905@gmail.com>
2024-04-30 14:23:29 -0300

    fix: adjusted error validation

--- src/api/services/products/index.ts
+++ src/api/services/products/index.ts
@@ -1,5 +1,6 @@
 import { ProductType } from 'src/api/types/products';
 import Products from 'src/database/models/products';
+import { AppError } from 'src/utils/errors';
 
 export class ProductService {
 
@@ -14,11 +15,11 @@ export class ProductService {
   async createProduct({ name, description, image }: ProductType): Promise<Products> {
     const productsExists = await this.findByName(name);
     if (productsExists) {
-      throw new Error('Product already exists');
+      throw AppError('Product already exists');
     }
 
     if (!name || !description || !image) {
-      throw new Error('All fields are required');
+      throw AppError('All fields are required');
     }
 
     return await Products.create({ name, description, image });
@@ -27,7 +28,7 @@ export class ProductService {
   async productById(id: number): Promise<Products> {
     const product = await Products.findByPk(id);
     if (!product) {
-      throw new Error('Product not found');
+      throw AppError('Product not found');
     }
 
     return product;
@@ -36,12 +37,12 @@ export class ProductService {
   async updateProduct({ id, name, description, image}: ProductType): Promise<number | Products> {
     const productExists = await this.findByName(name);
     if (productExists && productExists.id !== id) {
-      throw new Error('Product already exists');
+      throw AppError('Product already exists');
     }
 
     const productId = await this.productById(Number(id));
     if (!productId) {
-      throw new Error('Product already exists');
+      throw AppError('Product already exists');
     }
 
     return await productId!.update({ name, description, image });
@@ -50,7 +51,7 @@ export class ProductService {
   async deleteProduct(id: number): Promise<void> {
     const productId = await this.productById(Number(id));
     if (!productId) {
-      throw new Error('Product not found');
+      throw AppError('Product not found');
     }
 
     await productId?.destroy();
