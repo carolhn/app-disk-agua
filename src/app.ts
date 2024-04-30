6a06ffca85fc921c4de61d9a68c38659a81182e9
Caroline Nunes <nunescaroline905@gmail.com>
2024-04-30 14:23:03 -0300

    feat: create middleware error

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import { errors } from 'celebrate';
 import cors from 'cors';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import routes from './api/routes';
+import { AppError, AppErrorType } from './utils/errors';
 
 const app = express();
 
@@ -11,5 +12,15 @@ app.use(express.json())
 app.use(routes);
 
 app.use(errors())
+
+app.use((error: AppErrorType, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    });
+  }
+  return res.status(500).json({ status: 'error', message: 'Internal Server Error' });
+})
 
 export default app;
