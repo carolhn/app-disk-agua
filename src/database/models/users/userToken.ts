cdaf08a5c1c608ad775d14d25f881fd6adc6e5f8
Caroline Nunes <nunescaroline905@gmail.com>
2024-04-30 16:43:43 -0300

    feat: create `userToken` model

---
+++ src/database/models/users/userToken.ts
@@ -0,0 +1,33 @@
+import sequelize, { Model } from 'sequelize';
+import db from '..';
+
+class UserToken extends Model {
+  declare id: Number;
+  declare token: String;
+  declare userId: Number;
+}
+
+UserToken.init(
+  {
+    id: {
+      type: sequelize.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    token: {
+      type: sequelize.STRING(128),
+      autoIncrement: true,
+      allowNull: false,
+    },
+    userId: {
+      type: sequelize.INTEGER.UNSIGNED,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: 'user_tokens',
+    sequelize: db,
+    timestamps: false,
+  }
+)
