0155d5486065e4df2a574d4376568f909d7a819b
Caroline Nunes <nunescaroline905@gmail.com>
2024-05-17 20:37:56 -0300

    feat: adding authentication of the route

--- src/api/routes/users/index.ts
+++ src/api/routes/users/index.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import { UsersController } from 'src/api/controllers/users';
 import { validateCreate } from 'src/api/middleware/users';
-
+import { isAuthenticated } from 'src/api/middleware/users/isAuthenticated';
 
 const usersRouter = Router();
 const usersController = new UsersController();
 
 usersRouter.get('/', usersController.findAll);
 
-usersRouter.post('/', validateCreate(), usersController.createUser)
+usersRouter.post('/', validateCreate(), isAuthenticated, usersController.createUser)
 
 export default usersRouter;
