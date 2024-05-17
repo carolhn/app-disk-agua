c8a2421ac47d5ca6db5780a8298784ba4cc9cfbe
Caroline Nunes <nunescaroline905@gmail.com>
2024-05-17 18:16:15 -0300

    fix: adjusting function `AuthenticationService`

--- src/api/services/users/authentication.ts
+++ src/api/services/users/authentication.ts
@@ -1,31 +1,25 @@
+
+import { compare } from 'bcryptjs';
 import { UsersService } from 'src/api/services/users';
-import { AuthUser } from 'src/api/types/users';
-import UsersModel from 'src/database/models/users';
-import { CompareHash } from 'src/utils/authentication/crypto';
+import { RequestType, ResponseType } from 'src/api/types/users';
 import { AppError } from 'src/utils/errors';
 
 export class AuthenticationService {
 
-  async authenticationUser({ email, password }: AuthUser): Promise<UsersModel | null> {
-    const serviceUser = new UsersService();
+  async createAuthentication({ email, password }: RequestType): Promise<ResponseType> {
+    const users = new UsersService();
+    const usersData = await users.findByEmail(email);
 
-    try {
-      const user = await serviceUser.findByEmail(email);
-
-      if (!user) {
-        throw AppError('User not found', 404);
-      }
-
-      const passwordConfirmed = CompareHash(password, user.password);
-
-      if (!passwordConfirmed) {
-        throw AppError('Incorrect password', 401);
-      }
-
-      return user;
-    } catch (error) {
-      console.error(error);
-      throw AppError('Error during authentication', 500);
+    if (!usersData) {
+      throw AppError('Incorrect email/password combination', 401);
     }
+
+    const passwordConfirmed = await compare(password, usersData.password);
+
+    if (!passwordConfirmed) {
+      throw AppError('Incorrect email/password combination --- erro aqui', 401);
+    }
+
+    return { user: usersData };
   }
 }
