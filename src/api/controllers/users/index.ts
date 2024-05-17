0ad196b56d3df2c4f1d135fac4e7883912cb0f32
Caroline Nunes <nunescaroline905@gmail.com>
2024-05-17 18:15:15 -0300

    fix: updating file `UsersController`

--- src/api/controllers/users/index.ts
+++ src/api/controllers/users/index.ts
@@ -3,10 +3,10 @@ import { UsersService } from 'src/api/services/users';
 
 export class UsersController {
 
-  async findUsers(req: Request, res: Response): Promise<Response> {
+  async findAll(req: Request, res: Response): Promise<Response> {
     try {
       const users = new UsersService();
-      const allUsers = await users.findUsers();
+      const allUsers = await users.findAll();
       return res.status(200).json(allUsers);
     } catch (error) {
       return res.status(500).json({ message: 'Internal server error' });
@@ -15,10 +15,11 @@ export class UsersController {
 
   async createUser(req: Request, res: Response): Promise<Response> {
     const { name, email, password, avatar } = req.body;
+
     try {
       const users = new UsersService();
-      const newUser = await users.createUser({ name, email, password, avatar });
-      return res.status(201).json(newUser);
+      const createNewUser = await users.createUser({ name, email, password, avatar });
+      return res.status(201).json(createNewUser);
     } catch (error) {
       return res.status(500).json({ message: 'Internal server error' });
     }
