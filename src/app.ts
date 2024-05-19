6a3ddecdfae9eb8870478ad9a5bb83000ee13202
Caroline Nunes <nunescaroline905@gmail.com>
2024-05-19 12:52:22 -0300

    feat: create route upload

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import { errors } from 'celebrate';
 import cors from 'cors';
 import express from 'express';
 import { handleErrors } from 'src/api/middleware/error';
+import uploadConfig from 'src/utils/multer';
 import routes from './api/routes';
 
 const app = express();
 
 app.use(cors())
 app.use(express.json())
+app.use('/files', express.static(uploadConfig.directory))
 
 app.use(routes);
 
