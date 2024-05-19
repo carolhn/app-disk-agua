3c8daf0372273ab32e352cae56acacaaa801ddf0
Caroline Nunes <nunescaroline905@gmail.com>
2024-05-19 12:33:10 -0300

    feat: create router upload image avatar

--- src/api/routes/users/index.ts
+++ src/api/routes/users/index.ts
@@ -1,13 +1,24 @@
 import { Router } from 'express';
+import multer from 'multer';
 import { UsersController } from 'src/api/controllers/users';
 import { isAuthenticated } from 'src/api/middleware/authentication';
 import { validateCreate } from 'src/api/middleware/users';
+import uploadConfig from 'src/utils/multer';
 
 const usersRouter = Router();
 const usersController = new UsersController();
 
+const uploadPath = multer(uploadConfig);
+
 usersRouter.get('/', isAuthenticated, usersController.findAll);
 
 usersRouter.post('/', validateCreate(), isAuthenticated, usersController.createUser)
+
+usersRouter.patch(
+  '/avatar',
+  isAuthenticated,
+  uploadPath.single('avatar'),
+  usersController.updateUserAvatar
+);
 
 export default usersRouter;
